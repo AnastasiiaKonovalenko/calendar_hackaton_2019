c3ac74b11e6017c4fd153776b1e2a51111f1f249
natya2203 <nastya_2203@ukr.net>
2020-01-19 06:25:13 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+import DateDropdown from './components/dateDropdown/DateDropdown';
+import './App.css';
+import MonthArr from './monthArray';
+
+const App = () => {
+  return (
+    <DateDropdown />
+  );
+};
+export default App;

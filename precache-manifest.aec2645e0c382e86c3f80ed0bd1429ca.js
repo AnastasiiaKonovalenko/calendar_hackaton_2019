6393212dc28dec131a41f5a8bf5fa3d47365c8b1
ksukhaldun <kseniya.khaldun@gmail.com>
2020-01-19 09:43:35 +0300

    Updates

---
+++ precache-manifest.aec2645e0c382e86c3f80ed0bd1429ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28a45d686d5ecb89fb5e68b0b4284b9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d5cd473812d445dd7d0",
+    "url": "/static/css/main.501a818d.chunk.css"
+  },
+  {
+    "revision": "744fd97aaba3707f450b",
+    "url": "/static/js/2.e3726ea6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.e3726ea6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6d5cd473812d445dd7d0",
+    "url": "/static/js/main.334de36a.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  }
+]);
